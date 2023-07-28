3a105936a6598e3ccc1b612e4a314dd1387d8885
elieoko <elieoko100@gmail.com>
2023-07-28 14:11:29 +0100

    build:homepage

---
+++ app/src/main/java/com/example/volley/volley/UserSingleton.kt
@@ -0,0 +1,26 @@
+package com.example.volley.volley
+
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import android.content.Context
+import com.android.volley.toolbox.Volley
+
+class UserSingleton constructor(context: Context){
+
+    companion object {
+        @Volatile
+        private var INSTANCE: UserSingleton? = null
+        fun getInstance(context: Context) =
+            INSTANCE ?: synchronized(this) {
+                INSTANCE ?: UserSingleton(context).also {
+                    INSTANCE = it
+                }
+            }
+    }
+    private val requestQueue: RequestQueue by lazy {
+        Volley.newRequestQueue(context.applicationContext)
+    }
+    fun <T> addToRequestQueue(req: Request<T>) {
+        requestQueue.add(req)
+    }
+}
